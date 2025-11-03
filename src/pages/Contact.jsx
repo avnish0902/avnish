eeeb676b69e3bb48fb355561f574a03e7301a7fa
piyush9850 <piyushchoudhary0905@gmail.com>
2025-11-03 21:33:43 +0530

    content about me

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,4 +1,7 @@
 import ContactSection from "./Contact";
+import { Instagram, Linkedin, Mail, MapPin, Phone, Twitch, Twitter } from "lucide-react";
+
+
 
 export default function Contact() {
   return (
@@ -7,6 +10,90 @@ export default function Contact() {
         <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">
           Get In <span className="text-primary">Touch</span>
         </h2>
+
+        <p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto">
+          Have a project in mind or want to collaborate? Feel Free to reach out.
+          I'm always open to discussing new opportunites.
+        </p>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
+          <div className="space-y-8">
+            <h3 className="text-2xl font-semibold mb-6">
+              {" "}
+              Contact Information
+            </h3>
+
+            <div className = "space-y-6 justify-center">
+              <div className="fex items-start space-x-4">
+                <div className="p-3 rounded-full bg-primary/10">
+                <Mail className="h-6 w-6 text-primary" />{" "}
+                
+                </div>
+                <div>
+                  <h4 className="font-medium"> Email</h4>
+                  <a 
+                   href="mailto:hello@gmail.com"
+                   className="text-muted-foreground hover:text-primary transition-colors">
+                    hello@gmail.com
+                   </a>
+                </div>
+              </div>
+              <div className="fex items-start space-x-4">
+                <div className="p-3 rounded-full bg-primary/10">
+                <Phone className="h-6 w-6 text-primary" />{" "}
+                
+                </div>
+                <div>
+                  <h4 className="font-medium"> Phone</h4>
+                  <a 
+                   href="tel:+913563277246"
+                   className="text-muted-foreground hover:text-primary transition-colors">
+                    +91 (3563277246)
+                   </a>
+                </div>
+              </div>
+              <div className="fex items-start space-x-4">
+                <div className="p-3 rounded-full bg-primary/10">
+                <MapPin className="h-6 w-6 text-primary" />{" "}
+                
+                </div>
+                <div>
+                  <h4 className="font-medium"> Location</h4>
+                  <a 
+                 
+                   className="text-muted-foreground hover:text-primary transition-colors">
+                    IIIT Butibori,nagpur,pakistan
+                   </a>
+                </div>
+              </div>
+            </div>
+
+            <div className = "pt-8">
+              <h4 className="font-medium mb-4">Connet with Me</h4>
+              <div className="flex space-x-4 justify-center">
+                <a href="#" target = "_blank">
+                  <Linkedin />
+
+                </a>
+                <a href="#" target = "_blank">
+                  <Twitter />
+
+                </a>
+                <a href="#" target = "_blank">
+                  <Instagram />
+
+                </a><a href="#" target = "_blank">
+                  <Twitch />
+
+                </a>
+
+              </div>
+
+            </div>
+          </div>
+
+          
+        </div>
       </div>
     </section>
   );
